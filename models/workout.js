89ea02aa144a50997ca99cf7c1770fbc3dda325c
highwolfx <seanchangx@gmail.com>
2021-02-09 22:18:41 -0800

    Fixed seeding by adding index.js to point to workout.js

---
+++ models/workout.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        routine: [{
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter an exercise type."
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter an exercise name."
+            },
+            duration: {
+                type: Number,
+                required: "Please enter an exercise duration in minutes."
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+            }
+        }]
+    }
+);
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
@@ -7,8 +8,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./routes/api-routes")(app);
-require("./routes/html-routes")(app);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+require("./routes/api-routes.js")(app);
+// require("./routes/html-routes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
